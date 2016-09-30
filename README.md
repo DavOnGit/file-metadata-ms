49c9a058382fba47a057bc4abbf8b09b2647a180
Daviz <davide.vozzi@gmail.com>
2016-09-30 23:10:47 +0000

    Add App files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# file-metadata-ms
-File Metadata microservice
+[![Website](https://img.shields.io/website-up-down-green-red/https/file-metadata-parser.herokuapp.com%2F.svg?maxAge=2592000)](https://hero-tut.herokuapp.com/)
+[![StackShare](http://img.shields.io/badge/tech-stack-0690fa.svg?style=flat)](http://stackshare.io/DavOnGit/myown)
+[![License](https://img.shields.io/cocoapods/l/AFNetworking.svg)](http://doge.mit-license.org)
+
+# File Metadata microservice
+
+This simple App exract metadata from the uploaded file.
---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var express = require('express'),
+    fs      = require('fs'),
+    multer  = require('multer'),
+    app     = express();
+    
+var storage = multer.memoryStorage()
+var upload  = multer(
+    {
+        storage: storage,
+        limits: {
+            fileSize: 10240
+        }
+    }
+)
+                
+app.set('strict routing', true);
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('json spaces', 4);
+
+
+var type = upload.single('blob');
+
+app.post('/upload', type, function(req,res) {
+    
+    var sendBack = {
+        'Name': req.file.originalname,
+        'File size': req.file.size,
+        'Type': req.file.mimetype,
+        'Encoding': req.file.encoding
+    };
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.send(sendBack);
+    
+});
+
+
+app.use(function (err, req, res, next) {
+    console.log(err); // <-- this should show you the fieldname of the offending file
+    console.log(err.stack);
+    
+    if(err) {
+        res.status(400).end('Error: ' + err.message + '\n\nerror code: ' + err.code);
+    }
+});
+
+
+app.listen( app.get('port'), () => {
+    console.log('listening on port: ' + app.get('port'));
+});
